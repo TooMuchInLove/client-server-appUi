ab0a894b5ca1fa0b764c0993bf1275353db3a101
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-07 01:29:10 +0300

    update main

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ from PyQt5.QtWidgets import QWidget
 # Локальный config.py и классы
 from config import *
 from chatUI import ChatUI
+from client import Client
 
 
 if __name__ == '__main__':
   # Создание приложения
   app = QApplication(sys.argv)
   wig = QWidget()
-  # Экземпляр объекта класса
-  rui = ChatUI(wig, WIDTH_MIN, WIDTH_MAX, HEIGHT_MIN, HEIGHT_MAX)
+  # Пользовательский интерфейс GUI
+  gui = ChatUI(wig, WIDTH_MIN, WIDTH_MAX, HEIGHT_MIN, HEIGHT_MAX)
+  # Логика приложения
+  client = Client(gui)
   # Отобразить главное окно
   wig.show()
   # Конец приложения (закрытие)
